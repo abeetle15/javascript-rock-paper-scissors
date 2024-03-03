6be9392ec3202e8acf1da432a23fe340f7fc5001
A Beetle <abeetle15@gmail.com>
2024-03-02 19:43:12 -0500

    Create piedra papel o tijera.
     add HTML CSS and JavaScript. for now most of the JS is inside
     the buttons in a really horrible way but I don't know about
     loops yet so I can only trigger the code when buttons are clicked
     through "onclick". really hacky and needs to be solved. also I
     don't know yet how to make java print something on the screen so
     the webpage comunicates through alert messages which also sucks.
     fix this once you learn how to not suck. xoxo

---
+++ javascript.js
@@ -0,0 +1,10 @@
+  let userHand;
+  console.log("userHand = ''")
+
+  let computerHand;
+  console.log("computerHand = ''")
+
+  let computerHandValue;
+
+
+  // document.getElementById("computer-hand-display").innerHTML = tHe CoMpUtEr AwAiTs YoUr ChOiCe
